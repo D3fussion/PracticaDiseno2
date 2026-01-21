5c8540f933ac3c37077eeef465213c9c7bb96815
JUAN DIEGO QUIJADA CASTILLO <128547155+D3fussion@users.noreply.github.com>
2026-01-20 23:18:48 -0700

    feat: agregar página de inicio inicial, configuración de Tailwind CSS y recursos esenciales.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"], // Esto le dice dónde buscar tus clases
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
